26e9c10da59fe563d6de2cd3040d0c5a70238d93
Edwin de Jonge <edwindjonge@gmail.com>
2025-07-23 13:30:02 +0000

    version 1.1.2

--- inst/doc/inspect_mip.R
+++ inst/doc/inspect_mip.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -15,7 +15,7 @@ rules$exprs()
 rules <- validator(example_2 = if (has_house == "yes") income >= 1000)
 rules$exprs()
 
-## ---- df_print = "kable"------------------------------------------------------
+## ----df_print = "kable"-------------------------------------------------------
 rules <- validator( r1 = age >= 18
                   , r2 = income >= 0
                   )
@@ -36,13 +36,13 @@ mip <- inspect_mip(data, rules)
 lp <- mip$to_lp()
 print(lp)
 
-## ---- ref.label="mip1", comment= " ", echo=FALSE------------------------------
+## ----ref.label="mip1", comment= " ", echo=FALSE-------------------------------
 # inspect the lp problem (prior to solving it with lpsolveAPI)
 lp <- mip$to_lp()
 print(lp)
 
-## ---- eval=FALSE--------------------------------------------------------------
-#  mip$write_lp("my_problem.lp")
+## ----eval=FALSE---------------------------------------------------------------
+# mip$write_lp("my_problem.lp")
 
 ## -----------------------------------------------------------------------------
 res <- mip$execute()
@@ -66,7 +66,7 @@ res$values
 # lp problem after solving
 res$lp
 
-## ---- ref.label="mip2", comment=" ", echo=FALSE-------------------------------
+## ----ref.label="mip2", comment=" ", echo=FALSE--------------------------------
 # lp problem after solving
 res$lp
 
@@ -74,14 +74,14 @@ res$lp
 rules <- validator( r1 = working %in% c("job","retired")                  )
 data <- data.frame(working="?")
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::kable(data)
 
 ## ----mip3, results="hide"-----------------------------------------------------
 mip <- inspect_mip(data, rules)
 mip$to_lp()
 
-## ---- ref.label="mip3", comment=" ", echo=FALSE-------------------------------
+## ----ref.label="mip3", comment=" ", echo=FALSE--------------------------------
 mip <- inspect_mip(data, rules)
 mip$to_lp()
 
@@ -92,14 +92,14 @@ mip$execute()$values
 rules <- validator( r1 = if (voted == TRUE) adult == TRUE)
 data <- data.frame(voted = TRUE, adult = FALSE)
 
-## ---- echo=FALSE--------------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 knitr::kable(data)
 
 ## ----mip4, results="hide"-----------------------------------------------------
 mip <- inspect_mip(data, rules)
 mip$to_lp()
 
-## ---- ref.label="mip4", comment=" ", echo=FALSE-------------------------------
+## ----ref.label="mip4", comment=" ", echo=FALSE--------------------------------
 mip <- inspect_mip(data, rules)
 mip$to_lp()
 
@@ -110,7 +110,7 @@ mip$execute()$values
 rules <- validator( r1 = if (income > 0) age >= 16)
 data <- data.frame(age = 12, income = 2000)
 
-## ---- echo = FALSE------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 knitr::kable(data)
 
 ## -----------------------------------------------------------------------------
@@ -122,7 +122,7 @@ mip$mip_rules()
 ## ----mip6, results="hide"-----------------------------------------------------
 mip$to_lp()
 
-## ---- ref.label="mip6", comment=" ", echo=FALSE-------------------------------
+## ----ref.label="mip6", comment=" ", echo=FALSE--------------------------------
 mip$to_lp()
 
 ## -----------------------------------------------------------------------------
@@ -151,6 +151,6 @@ mip$objective
 ## ----mip7, results="hide"-----------------------------------------------------
 mip$to_lp()
 
-## ---- ref.label="mip7", comment=" ", echo=FALSE-------------------------------
+## ----ref.label="mip7", comment=" ", echo=FALSE--------------------------------
 mip$to_lp()
 
